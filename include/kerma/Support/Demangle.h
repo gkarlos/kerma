c5219dcf9a417eb13c9954b125fa193eaca230c1
gkarlos <gkarloscodes@gmail.com>
2020-02-08 14:33:31 +0100

    use __cxa_demangle for older LLVM versions

---
+++ include/kerma/Support/Demangle.h
@@ -0,0 +1,15 @@
+#ifndef KERMA_STATIC_ANALYSIS_SUPPORT_DEMANGLE_H
+#define KERMA_STATIC_ANALYSIS_SUPPORT_DEMANGLE_H
+
+#include <llvm/IR/Function.h>
+
+namespace kerma
+{
+
+std::string demangleFn(llvm::Function *f);
+
+std::string demangleFnWithoutArgs(llvm::Function *f);
+
+} /// NAMESPACE kerma
+
+#endif
---
+++ src/Support/Demangle.cpp
@@ -0,0 +1,35 @@
+#include <kerma/Support/Demangle.h>
+
+#include <llvm/Config/llvm-config.h>
+
+
+#if LLVM_VERSION_MAJOR < 9
+#include <cxxabi.h>
+#else
+#include <llvm/Demangle/Demangle.h>
+#endif
+
+
+std::string kerma::demangleFn(llvm::Function *f) {
+
+#if LLVM_VERSION_MAJOR < 9
+  int status;
+  char *p = abi::__cxa_demangle( f->getName().str().c_str(), nullptr, nullptr, &status);
+  if ( status != 0)
+    return std::string(f->getName().str().append("_(could not demangle)"));
+  std::string demangled(p);
+  free(p);
+  return demangled;
+#else
+  return llvm::demangle(f->getName().str());
+#endif
+}
+
+std::string kerma::demangleFnWithoutArgs(llvm::Function *f)
+{
+  std::string demangledName = kerma::demangleFn(f); //llvm::demangle(std::string(f->getName().str()));
+  if ( auto argstart = demangledName.find('(')) {
+    demangledName = demangledName.substr(0, argstart);
+  }
+  return demangledName;
+}
