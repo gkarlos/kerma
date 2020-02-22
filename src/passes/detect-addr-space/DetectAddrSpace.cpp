dc10548f823df3e4e54918aff88c3cefe57a3378
gkarlos <gkarloscodes@gmail.com>
2020-02-22 20:59:12 +0100

    [Pass][DetectAddrSpace] files up

---
+++ src/passes/detect-addr-space/DetectAddrSpace.cpp
@@ -0,0 +1,47 @@
+#include "kerma/Support/NVVM.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+#include <kerma/passes/detect-addr-space/DetectAddrSpace.h>
+#include <kerma/passes/detect-kernels/DetectKernels.h>
+
+using namespace llvm;
+
+
+/// Register Pass
+char kerma::DetectAddrSpacePass::ID = 1;
+static RegisterPass<kerma::DetectAddrSpacePass> INIT_DETECT_ADDR_SPACE("kerma-detect-addr-space", "Detect Address Space of Loads and Stores", false, true);
+
+
+namespace kerma
+{
+
+void DetectAddrSpacePass::getAnalysisUsage(AnalysisUsage& AU) const 
+{
+  AU.setPreservesAll();
+  AU.addRequired<DetectKernelsPass>();
+}
+
+bool DetectAddrSpacePass::runOnModule(llvm::Module &M) 
+{
+  auto kernels = &getAnalysis<DetectKernelsPass>().getKernels();
+
+  for ( auto* kernel : *kernels) {
+    outs() << "Detect-Address-Space: " << kernel->getFn()->getName() << "\n";
+  }
+
+  return false;
+}
+
+AddressSpace DetectAddrSpacePass::getAddrSpace(llvm::Value *v)
+{
+  if ( auto load = dyn_cast<LoadInst>(v)) {
+    return getAddrSpace(load->getPointerOperand());
+  }
+}
+
+}
+
+
+
+
