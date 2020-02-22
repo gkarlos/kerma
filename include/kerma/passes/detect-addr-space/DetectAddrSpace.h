20db4b52584851c2af85c657953a41737271af33
gkarlos <gkarloscodes@gmail.com>
2020-02-22 20:59:12 +0100

    [Pass][DetectAddrSpace] files up

---
+++ include/kerma/passes/detect-addr-space/DetectAddrSpace.h
@@ -0,0 +1,33 @@
+#ifndef KERMA_STATIC_ANALYSIS_DETECTADDRESSSPACE_H
+#define KERMA_STATIC_ANALYSIS_DETECTADDRESSSPACE_H
+
+
+#include "kerma/Support/NVVM.h"
+#include <llvm/Pass.h>
+#include <llvm/IR/Value.h>
+
+namespace kerma
+{
+
+class DetectAddrSpacePass : public llvm::ModulePass {
+
+public:
+  static char ID;
+  DetectAddrSpacePass() : llvm::ModulePass(ID)
+  {}
+
+  bool runOnModule(llvm::Module &M) override;
+  void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
+
+  // API
+
+  AddressSpace getAddrSpace(llvm::Value *v);
+
+  bool maybeGlobal(llvm::Value *v);
+
+  bool provablyGlobal(llvm::Value *v);
+};
+
+}
+
+#endif /* KERMA_STATIC_ANALYSIS_DETECTADDRESSSPACE_H */
