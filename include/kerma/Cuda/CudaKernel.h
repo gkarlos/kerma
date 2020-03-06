c2197601dc3983fdd08c59c2b77442a8edf1e140
gkarlos <gkarloscodes@gmail.com>
2020-03-06 18:36:32 +0100

    [test] Add more tests to TestCudaKernel

--- include/kerma/Cuda/CudaKernel.h
+++ include/kerma/Cuda/CudaKernel.h
@@ -1,7 +1,5 @@
 /*
- * @file: include/kerma/Support/Cuda.h
- *
- * Includes various wrappers for CUDA related things
+ * @file: include/Cuda/CudaKernel.h
  */
 #ifndef KERMA_SUPPORT_CUDA_H
 #define KERMA_SUPPORT_CUDA_H
@@ -78,49 +76,123 @@ public:
   std::string& getMangledName();
 
   /*
-   * Set the line number at which the signature of the kernel's definition starts
+   * @brief Set the line number at which the signature of this CudaKernel's definition starts
+   * 
+   * if @p line > @pre #signatureLineEnd_, then @post #signatureLineEnd_ is set to #SRC_LINE_UNKNOWN
+   *
+   * @post signatureLineStart_ <= signatureLineEnd_ || signatureLineEnd_ == SRC_LINE_UNKNOWN
    */
   void setSignatureLineStart(unsigned int line);
 
   /*
-   * Set the line number at which the signature of the kernel's definition ends
+   * @brief Set the line number at which the signature of this CudaKernel's definition ends
+   *
+   * if @p line < @pre #signatureLineStart_, then @post #signatureLineEnd_ is set to #SRC_LINE_UNKNOWN
+   *
+   * @post signatureLineStart_ <= signatureLineEnd_ || signatureLineEnd_ == SRC_LINE_UNKNOWN
    */
   void setSignatureLineEnd(unsigned int line);
 
+  /*
+   * @brief Set the start line and end line of this CudaKernel's signature in source code
+   *        Equivalent to calling #setSignatureLineStart() followed by #setSignatureLineEnd()
+   */
   void setSignatureLines(unsigned int start, unsigned int end);
 
-  unsigned int getSignatureLineStart();
+  /*
+   * @brief Retrieve the start line of this CudaKernel's signature in source code
+   * @return An integer >= 0 or SRC_LINE_UNKNOWN
+   */
+  int getSignatureLineStart();
 
-  unsigned int getSignatureLineEnd();
+  /*
+   * @brief Retrieve the end line of this CudaKernel's signature in source code
+   * @return An integer >= 0 or SRC_LINE_UNKNOWN
+   */
+  int getSignatureLineEnd();
 
-  unsigned int getSignatureNumLines();
+  /*
+   * @brief Retrieve the number of lines this CudaKernel's signature spans over (inclusive)
+   * 
+   * The number of lines is calculated as <code> 1 + #signatureLineEnd_ - #signatureLineStart_ </code>
+   *
+   * If any of #signatureLineStart_ or #signatureLineEnd_ have the value #SRC_LINE_UNKNOWN, then the
+   * number of lines cannot be calculated and 0 is returned.
+   * 
+   * @return A positive integer or 0
+   */
+  int getSignatureNumLines();
 
+  /*
+   * @brief Set the line number of which the body of this CudaKernel's definition starts
+   *
+   * if @p line < @pre #signatureLineEnd_, then @post #bodyLineStart_ is set to #SRC_LINE_UNKNOWN
+   * if @p line > @pre #bodyLineEnd_, then @post #bodyLineEnd_ is set to #SRC_LINE_UNKNOWN
+   *
+   * @post #signatureLineEnd_ <= #bodyLineStart <= #bodyLineEnd ||
+   *       #bodyLineStart >= #signatureLineEnd_ && #bodyLineEnd_ == #SRC_LINE_UNKNOWN ||
+   *       #bodyLineStart == #bodyLineEnd == #SRC_LINE_UNKNOWN
+   */
   void setBodyLineStart(unsigned int line);
 
+  /*
+   * @brief Set the line number of which the body of this CudaKernel's definition ends
+   */
   void setBodyLineEnd(unsigned int line);
 
+  /*
+   * @brief Set the start line and end line of this CudaKernel's body in source code
+   *        Equivalent to calling #setBodyLineStart() followed by #setBodyLineEnd()
+   */
   void setBodyLines(unsigned int start, unsigned int end);
 
-  unsigned int getBodyLineStart();
-
-  unsigned int getBodyLineEnd();
-
-  unsigned int getBodyNumLines();
+  /*
+   * @brief Retrieve the start line of this CudaKernel's body in source code
+   * @return An integer >= 0 or SRC_LINE_UNKNOWN
+   */
+  int getBodyLineStart();
 
   /*
-   * Retrieve the first (source code) line of the kernel's definition
+   * @brief Retrieve the end line of this CudaKernel's body in source code
+   * @return An integer >= 0 or SRC_LINE_UNKNOWN
    */
-  unsigned int getLineStart();
+  int getBodyLineEnd();
+
+  /*
+   * @brief Retrieve the number of lines this CudaKernel's body spans over (inclusive)
+   * 
+   * The number of lines is calculated as <code> 1 + #bodyLineEnd_ - #bodyLineStart_ </code>
+   *
+   * If any of #bodyLineStart_ or #signatureLbodyLineEnd have the value #SRC_LINE_UNKNOWN, then the
+   * number of lines cannot be calculated and 0 is returned.
+   * 
+   * @return A positive integer or 0
+   */
+  int getBodyNumLines();
+
+  /*
+   * @brief Retrieve the first (source code) line of the kernel's definition
+   * 
+   * Equivalent to #getSignatureLineStart() 
+   */
+  int getLineStart();
   
   /*
-   * Retrieve the last (source code) line of the kernel's definition
+   * @brief Retrieve the last (source code) line of the kernel's definition
+   *
+   * Equivalent to #getBodyLineEnd()
    */
-  unsigned int getLineEnd();
+  int getLineEnd();
 
   /*
-   * Retrieve the number of source code this kernel function spans
+   * @brief Retrieve the number of source code this kernel function spans
+   *
+   * If any of #signatureLineStart_ or bodyLineEnd_ == #SRC_LINE_UNKNOWN, then the number
+   * of lines cannot be calculated and 0 is returned.
+   *
+   * @return A positive integer or 0
    */
-  unsigned int getNumLines();
+  int getNumLines();
 };
 
 class CudaKernelLaunchConfiguration
--- test/unit/lib/Cuda/TestCudaKernel.cpp
+++ test/unit/lib/Cuda/TestCudaKernel.cpp
@@ -5,6 +5,7 @@
 #include "kerma/Cuda/CudaModule.h"
 #include "kerma/Pass/DetectKernels.h"
 #include "kerma/Support/Demangle.h"
+#include "kerma/Support/SourceCode.h"
 
 #include "llvm/IR/Function.h"
 #include "llvm/Support/SourceMgr.h"
@@ -20,28 +21,6 @@ using namespace llvm;
 using namespace kerma;
 
 #define POLYBENCH std::string("../../../../../examples/polybench/cuda/")
-
-DetectKernelsPass *
-runDetectKernelsPass(const std::string& hostIR, const std::string& deviceIR)
-{
-  LLVMContext context;
-  SMDiagnostic error;
-  auto hostModule = llvm::parseIRFile(hostIR, error, context);
-  auto deviceModule = llvm::parseIRFile(deviceIR, error, context);
-  CudaModule module(*hostModule.get(), *deviceModule.get());
-
-  legacy::PassManager PM;
-  DetectKernelsPass *dkp = new DetectKernelsPass(module);
-  PM.add(dkp);
-  PM.run(module.getDeviceModule());
-  return dkp;
-}
-
-void cleanupPass(DetectKernelsPass **dkp)
-{
-  delete *dkp;
-  *dkp = nullptr;
-}
 
 std::unique_ptr<llvm::Module>
 readLLVMModule(const std::string& IRFile)
@@ -129,11 +108,9 @@ TEST(Constructor, Function_CudaSide)
     ASSERT_EQ(kernel.getMangledName().compare(kernelF->getName()), 0);
     ASSERT_EQ(kernel.getNumArgs(), kernelF->arg_size());
   }
-    
-  // cleanupPass(&dkp);
 }
 
-TEST(LineNumber, All )
+TEST(LineNumbers, Default )
 {
   std::string deviceIR = POLYBENCH + "2MM/2mm-cuda-nvptx64-nvidia-cuda-sm_52.ll";
   LLVMContext context;
@@ -141,20 +118,270 @@ TEST(LineNumber, All )
   auto deviceModule = parseIRFile(deviceIR, error, context);
 
   for ( auto &F : *deviceModule.get()) {
-    CudaKernel f(F, CudaSide::DEVICE);
-    ASSERT_EQ(f.getLineStart(), 0);
-    ASSERT_EQ(f.getLineEnd(), 0);
-    ASSERT_EQ(f.getNumLines(), 0);
+    CudaKernel A(F, CudaSide::DEVICE);
+    ASSERT_EQ(A.getSignatureLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getSignatureLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getSignatureNumLines(), 0);
+    ASSERT_EQ(A.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getBodyNumLines(), 0);
+    ASSERT_EQ(A.getLineStart(), A.getSignatureLineStart());
+    ASSERT_EQ(A.getLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getLineEnd(), A.getBodyLineEnd());
+    ASSERT_EQ(A.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getNumLines(), 0);
+  }
+}
 
-    f.setSignatureLineStart(10);
-    ASSERT_EQ(f.getLineStart(), 10);
+TEST( LineNumbers, SetSignatureValidStartEnd )
+{
+  std::string deviceIR = POLYBENCH + "2MM/2mm-cuda-nvptx64-nvidia-cuda-sm_52.ll";
+  LLVMContext context;
+  SMDiagnostic error;
+  auto deviceModule = parseIRFile(deviceIR, error, context);
 
-    f.setBodyLineEnd(15);
-    ASSERT_EQ(f.getLineEnd(), 15);
+  for ( auto &F : *deviceModule.get()) {
+    CudaKernel A(F, CudaSide::DEVICE);
 
-    ASSERT_EQ(f.getNumLines(), 5);
+    A.setSignatureLineStart(0);
+    A.setSignatureLineEnd(1);
+    ASSERT_EQ(A.getSignatureLineStart(), 0);
+    ASSERT_EQ(A.getSignatureLineEnd(), 1);
+    ASSERT_EQ(A.getSignatureNumLines(), 2);
+    ASSERT_EQ(A.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getBodyNumLines(), 0);
+    ASSERT_EQ(A.getLineStart(), 0);
+    ASSERT_EQ(A.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ(A.getNumLines(), 0);
+  }
+}
 
-    f.setBodyLineEnd(0);
-    ASSERT_EQ(f.getNumLines(), 0);
+TEST( LineNumbers, SetLineNumbersFromFile )
+{
+  std::string deviceIR = POLYBENCH + "2MM/2mm-cuda-nvptx64-nvidia-cuda-sm_52.ll";
+  LLVMContext context;
+  SMDiagnostic error;
+  auto deviceModule = parseIRFile(deviceIR, error, context);
+
+  for ( auto &F : *deviceModule.get()) {
+    if ( demangleFnWithoutArgs(F).compare("mm2_kernel1") != 0 ) {
+      CudaKernel A(F, CudaSide::DEVICE);
+      A.setSignatureLineStart(108);
+      A.setSignatureLineEnd(108);
+      A.setBodyLineStart(108);
+      A.setBodyLineEnd(120);
+
+      ASSERT_EQ( A.getSignatureLineStart(), 108);
+      ASSERT_EQ( A.getSignatureLineEnd(), 108);
+      ASSERT_EQ( A.getBodyLineStart(), 108);
+      ASSERT_EQ( A.getBodyLineEnd(), 120);
+      ASSERT_EQ( A.getSignatureNumLines(), 1);
+      ASSERT_EQ( A.getBodyNumLines(), 13);
+      ASSERT_EQ( A.getLineStart(), A.getSignatureLineStart());
+      ASSERT_EQ( A.getLineStart(), 108);
+      ASSERT_EQ( A.getLineEnd(), A.getBodyLineEnd());
+      ASSERT_EQ( A.getLineEnd(), 120);
+      ASSERT_EQ( A.getNumLines(), A.getBodyNumLines());
+      ASSERT_EQ( A.getNumLines(), 13);
+
+      CudaKernel B(F, CudaSide::DEVICE);
+      A.setSignatureLines(108, 108);
+      A.setBodyLines(108, 120);
+
+      ASSERT_EQ( A.getSignatureLineStart(), 108);
+      ASSERT_EQ( A.getSignatureLineEnd(), 108);
+      ASSERT_EQ( A.getBodyLineStart(), 108);
+      ASSERT_EQ( A.getBodyLineEnd(), 120);
+      ASSERT_EQ( A.getSignatureNumLines(), 1);
+      ASSERT_EQ( A.getBodyNumLines(), 13);
+      ASSERT_EQ( A.getLineStart(), A.getSignatureLineStart());
+      ASSERT_EQ( A.getLineStart(), 108);
+      ASSERT_EQ( A.getLineEnd(), A.getBodyLineEnd());
+      ASSERT_EQ( A.getLineEnd(), 120);
+      ASSERT_EQ( A.getNumLines(), A.getBodyNumLines());
+      ASSERT_EQ( A.getNumLines(), 13);
+    }
+
+    if ( demangleFnWithoutArgs(F).compare("mm2_kernel2") != 0 ) {
+      CudaKernel A(F, CudaSide::DEVICE);
+      A.setSignatureLineStart(123);
+      A.setSignatureLineEnd(123);
+      A.setBodyLineStart(124);
+      A.setBodyLineEnd(136);
+
+      ASSERT_EQ( A.getSignatureLineStart(), 123);
+      ASSERT_EQ( A.getSignatureLineEnd(), 123);
+      ASSERT_EQ( A.getBodyLineStart(), 124);
+      ASSERT_EQ( A.getBodyLineEnd(), 136);
+      ASSERT_EQ( A.getSignatureNumLines(), 1);
+      ASSERT_EQ( A.getBodyNumLines(), 13);
+      ASSERT_EQ( A.getLineStart(), A.getSignatureLineStart());
+      ASSERT_EQ( A.getLineStart(), 123);
+      ASSERT_EQ( A.getLineEnd(), A.getBodyLineEnd());
+      ASSERT_EQ( A.getLineEnd(), 136);
+      ASSERT_EQ( A.getNumLines(), A.getBodyNumLines() + 1);
+      ASSERT_EQ( A.getNumLines(), 14);
+
+      CudaKernel B(F, CudaSide::DEVICE);
+      A.setSignatureLines(123, 123);
+      A.setBodyLines(124, 136);
+
+      ASSERT_EQ( A.getSignatureLineStart(), 123);
+      ASSERT_EQ( A.getSignatureLineEnd(), 123);
+      ASSERT_EQ( A.getBodyLineStart(), 124);
+      ASSERT_EQ( A.getBodyLineEnd(), 136);
+      ASSERT_EQ( A.getSignatureNumLines(), 1);
+      ASSERT_EQ( A.getBodyNumLines(), 13);
+      ASSERT_EQ( A.getLineStart(), A.getSignatureLineStart());
+      ASSERT_EQ( A.getLineStart(), 123);
+      ASSERT_EQ( A.getLineEnd(), A.getBodyLineEnd());
+      ASSERT_EQ( A.getLineEnd(), 136);
+      ASSERT_EQ( A.getNumLines(), A.getBodyNumLines() + 1);
+      ASSERT_EQ( A.getNumLines(), 14);
+    }
+  }
+}
+
+TEST( LineNumbers, SignatureInconsistencies )
+{
+  std::string deviceIR = POLYBENCH + "2MM/2mm-cuda-nvptx64-nvidia-cuda-sm_52.ll";
+  LLVMContext context;
+  SMDiagnostic error;
+  auto deviceModule = parseIRFile(deviceIR, error, context);
+
+  for ( auto &F : *deviceModule.get()) {
+    CudaKernel A(F, CudaSide::DEVICE);
+    A.setSignatureLines(123, 123);
+    A.setBodyLines(124, 136);
+
+    ASSERT_EQ( A.getSignatureLineStart(), 123);
+    ASSERT_EQ( A.getSignatureLineEnd(), 123);
+    ASSERT_EQ( A.getBodyLineStart(), 124);
+    ASSERT_EQ( A.getBodyLineEnd(), 136);
+    ASSERT_EQ( A.getSignatureNumLines(), 1);
+    ASSERT_EQ( A.getBodyNumLines(), 13);
+    ASSERT_EQ( A.getLineStart(), A.getSignatureLineStart());
+    ASSERT_EQ( A.getLineStart(), 123);
+    ASSERT_EQ( A.getLineEnd(), A.getBodyLineEnd());
+    ASSERT_EQ( A.getLineEnd(), 136);
+    ASSERT_EQ( A.getNumLines(), A.getBodyNumLines() + 1);
+    ASSERT_EQ( A.getNumLines(), 14);
+
+    A.setSignatureLineStart(125);
+    ASSERT_EQ( A.getSignatureLineStart(), 125);
+    ASSERT_EQ( A.getSignatureLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getLineStart(), 125);
+    ASSERT_EQ( A.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getSignatureNumLines(), 0);
+    ASSERT_EQ( A.getBodyNumLines(), 0);
+    ASSERT_EQ( A.getNumLines(), 0);
+
+    // Reset
+    CudaKernel B(F, CudaSide::DEVICE);
+    B.setSignatureLines(123, 123);
+    B.setBodyLines(124, 136);
+
+    B.setSignatureLineEnd(125);
+    ASSERT_EQ( B.getSignatureLineStart(), 123);
+    ASSERT_EQ( B.getSignatureLineEnd(), 125);
+    ASSERT_EQ( B.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( B.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( B.getLineStart(), 123);
+    ASSERT_EQ( B.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( B.getSignatureNumLines(), 3);
+    ASSERT_EQ( B.getBodyNumLines(), 0);
+    ASSERT_EQ( B.getNumLines(), 0);
+
+    // Reset
+    CudaKernel C(F, CudaSide::DEVICE);
+    C.setSignatureLines(123, 123);
+    C.setBodyLines(124, 136);
+
+    C.setSignatureLineStart(150);
+    ASSERT_EQ( C.getSignatureLineStart(), 150);
+    ASSERT_EQ( C.getSignatureLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( C.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( C.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( C.getLineStart(), 150);
+    ASSERT_EQ( C.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( C.getSignatureNumLines(), 0);
+    ASSERT_EQ( C.getBodyNumLines(), 0);
+    ASSERT_EQ( C.getNumLines(), 0);
+
+    // Reset
+    CudaKernel D(F, CudaSide::DEVICE);
+    D.setSignatureLines(123, 123);
+    D.setBodyLines(124, 136);
+
+    D.setSignatureLineEnd(150);
+    ASSERT_EQ( D.getSignatureLineStart(), 123);
+    ASSERT_EQ( D.getSignatureLineEnd(), 150);
+    ASSERT_EQ( D.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( D.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( D.getLineStart(), 123);
+    ASSERT_EQ( D.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( D.getSignatureNumLines(), 28);
+    ASSERT_EQ( D.getBodyNumLines(), 0);
+    ASSERT_EQ( D.getNumLines(), 0);
+  }
+}
+
+TEST( LineNumbers, BodyInconsistencies )
+{
+  std::string deviceIR = POLYBENCH + "2MM/2mm-cuda-nvptx64-nvidia-cuda-sm_52.ll";
+  LLVMContext context;
+  SMDiagnostic error;
+  auto deviceModule = parseIRFile(deviceIR, error, context);
+
+  for ( auto &F : *deviceModule.get()) {
+    CudaKernel A(F, CudaSide::DEVICE);
+    A.setSignatureLines(123, 123);
+    A.setBodyLines(124, 136);
+
+    A.setBodyLineStart(0);
+    ASSERT_EQ( A.getBodyLineStart(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getBodyNumLines(), 0);
+    ASSERT_EQ( A.getSignatureLineStart(), 123);
+    ASSERT_EQ( A.getSignatureLineEnd(), 123);
+    ASSERT_EQ( A.getSignatureNumLines(), 1);
+
+    ASSERT_EQ( A.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( A.getNumLines(), 0);
+
+    CudaKernel B(F, CudaSide::DEVICE);
+    B.setSignatureLines(123, 123);
+    B.setBodyLines(124, 136);
+
+    B.setBodyLineEnd(123);
+    ASSERT_EQ( B.getBodyLineStart(), 124);
+    ASSERT_EQ( B.getBodyLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( B.getBodyNumLines(), 0);
+    ASSERT_EQ( B.getLineEnd(), SRC_LINE_UNKNOWN);
+    ASSERT_EQ( B.getNumLines(), 0);
+
+    ASSERT_EQ( B.getSignatureLineStart(), 123);
+    ASSERT_EQ( B.getSignatureLineEnd(), 123);
+    ASSERT_EQ( B.getSignatureNumLines(), 1);
+
+    CudaKernel C(F, CudaSide::DEVICE);
+    C.setSignatureLines(123, 123);
+    C.setBodyLines(124, 136);
+
+    C.setBodyLineEnd(130);
+    ASSERT_EQ( C.getBodyLineStart(), 124);
+    ASSERT_EQ( C.getBodyLineEnd(), 130);
+    ASSERT_EQ( C.getBodyNumLines(), 7);
+    ASSERT_EQ( C.getLineEnd(), 130);
+    ASSERT_EQ( C.getNumLines(), 8);
+
+    ASSERT_EQ( C.getSignatureLineStart(), 123);
+    ASSERT_EQ( C.getSignatureLineEnd(), 123);
+    ASSERT_EQ( C.getSignatureNumLines(), 1);
+
+
   }
 }
