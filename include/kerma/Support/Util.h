e67c17526cdbe487d5479a4b833f0a571989c219
gkarlos <gkarloscodes@gmail.com>
2020-03-08 13:30:48 +0100

    [lib][Suppor] Added Util.h with logging utilities

---
+++ include/kerma/Support/Util.h
@@ -0,0 +1,16 @@
+#ifndef KERMA_SUPPORT_UTIL_H
+#define KERMA_SUPPORT_UTIL_H
+
+#include <iostream>
+
+#define ERROR(msg, fatal) \
+  do { \
+      std::cerr << "ERROR" + ((fatal)? " (fatal)" : "") + " at " << __FILE__ << ":" << __LINE__ << ", function " << __func__ << "():\n    "; \
+      std::cerr << msg << std::endl; \
+      if ( fatal) \
+        abort(); \
+  } while (false)
+
+#define FATAL(msg) ERROR(msg, true)
+
+#endif /* KERMA_SUPPORT UTIL_H */
