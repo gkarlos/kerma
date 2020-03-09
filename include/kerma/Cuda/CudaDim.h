d92c0723d6383efc43e6b756d73f8f3780ab0309
gkarlos <gkarloscodes@gmail.com>
2020-03-09 20:09:49 +0100

    [docs] Added some doc strings in CudaDim.h

--- include/kerma/Cuda/CudaDim.h
+++ include/kerma/Cuda/CudaDim.h
@@ -36,10 +36,22 @@ enum CudaDimError : int
 
 CudaDimError validateGrid(CudaDim &dim);
 
+/*!
+ * @brief Check if the grid dimensions are "correct" w.r.t some Compute Capability
+ * @param compute [in] a CudaCompute
+ * @param dim [in] a CudaDim for the grid
+ * @return CudaDimError indicating status
+ */
 CudaDimError validateGrid(CudaCompute &compute, CudaDim &dim);
 
 CudaDimError validateBlock(CudaDim &dim);
 
+/*!
+ * @brief Check if the block dimensions are "correct" w.r.t some Compute Capability
+ * @param compute [in] a CudaCompute
+ * @param dim [in] a CudaDim for the blocks
+ * @return CudaDimError indicating status
+ */
 CudaDimError validateBlock(CudaCompute &compute, CudaDim &dim);
 
 
