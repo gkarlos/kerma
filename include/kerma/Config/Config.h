832413150b3ac42cca574220c0573f8f4ccb4cdf
gkarlos <gkarloscodes@gmail.com>
2020-03-11 04:00:42 +0100

    [lib] Wrap around Config macros (cont.)

--- include/kerma/Config/Config.h
+++ include/kerma/Config/Config.h
@@ -1,6 +1,8 @@
-/*!
- * The main config header. Includes all other config headers
- * in this directory.
+/*! @file include/kerma/Config/Config.h
+ *
+ * The main config header. It is meant to be THE ONLY config header
+ * to include when config info is needed. It includes all other config 
+ * headers and provides wrappers to the macros they define.
  */
 #ifndef KERMA_CONFIG_CONFIG_H
 #define KERMA_CONFIG_CONFIG_H
@@ -8,14 +10,13 @@
 #include "version.h"
 #include "config.h"
 
-#include <cstdlib>
-
 namespace kerma
 {
 namespace config
 {
 namespace ver
 {
+  // Version
   static constexpr unsigned int stou(const char * str, int val = 0) {
     return (*str ? stou(str + 1, (*str - '0') + val * 10) : val);
   }
@@ -24,11 +25,18 @@ namespace ver
     return a + b / 100.0;
   }
 
+  
   constexpr unsigned int maj = stou(KERMA_MAJOR_VERSION);
   constexpr unsigned int min = stou(KERMA_MINOR_VERSION);
-  constexpr float ver = iconcat(maj, min);
-
+  constexpr float full = iconcat(maj, min);
 } /* NAMESPACE ver    */
+  
+// Tests
+constexpr bool testsEnabled = static_cast<bool>(KERMA_TESTS_ENABLED);
+
+// Examples
+constexpr bool examplesEnabled = static_cast<bool>(KERMA_EXAMPLES_ENABLED);
+
 } /* NAMESPACE config */
 } /* NAMESPACE kerma  */
 #endif /* KERMA_CONFIG_CONFIG_H */
--- include/kerma/Config/config.h
+++ include/kerma/Config/config.h
@@ -1,6 +1,7 @@
 #ifndef _KERMA_CONFIG_H_
 #define _KERMA_CONFIG_H_
 
-#define KERMA_TESTS_ENABLED
+#define KERMA_TESTS_ENABLED 1
+#define KERMA_EXAMPLES_ENABLED 1
 
 #endif /* _KERMA_CONFIG_H_ */
