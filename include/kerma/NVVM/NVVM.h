97f804412476d113919735c5d940e0c28815af91
George Karlos <gkarloscodes@gmail.com>
2020-08-28 23:56:07 +0200

    [nvvm] Added block/thread idx intrinsics

--- include/kerma/NVVM/NVVM.h
+++ include/kerma/NVVM/NVVM.h
@@ -30,9 +30,31 @@ public:
   const std::string z = "__cuda_builtin_blockDim_t::__fetch_builtin_z()";
 } BlockDim;
 
+struct {
+private:
+  const std::string v_ = "__cuda_builtin_blockIdx_t";
+public:
+  operator std::string() const { return v_; }
+  std::string operator+(std::string other) const { return *this + other; }
 
+  const std::string x = "__cuda_builtin_blockIdx_t::__fetch_builtin_x()";
+  const std::string y = "__cuda_builtin_blockIdx_t::__fetch_builtin_y()";
+  const std::string z = "__cuda_builtin_blockIdx_t::__fetch_builtin_z()";
+} BlockIdx;
 
-}
+struct {
+private:
+  const std::string v_ = "__cuda_builtin_threadIdx_t";
+public:
+  operator std::string() const { return v_; }
+  std::string operator+(std::string other) const { return *this + other; }
+
+  const std::string x = "__cuda_builtin_threadIdx_t::__fetch_builtin_x()";
+  const std::string y = "__cuda_builtin_threadIdx_t::__fetch_builtin_y()";
+  const std::string z = "__cuda_builtin_threadIdx_t::__fetch_builtin_z()";
+} ThreadIdx;
+
+} // namespace nvvm
 } // namespace kerma
 
 #endif // KERMA_NVVM_H
