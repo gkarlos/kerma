a37451e37ad582b4de1b03a357577faa82bd3aa2
George Karlos <gkarloscodes@gmail.com>
2020-08-31 14:21:17 +0200

    [SourceInfo] Make SourceLoc methods non-virtual

--- include/kerma/SourceInfo/SourceLoc.h
+++ include/kerma/SourceInfo/SourceLoc.h
@@ -16,24 +16,24 @@ public:
   SourceLoc(int line=0, int col=0);
   SourceLoc(const SourceLoc& other);
   SourceLoc(SourceLoc&& other);
-  virtual ~SourceLoc()=default;
+  ~SourceLoc()=default;
 
-  virtual int getLine() const;
-  virtual int getCol() const;
-  virtual SourceLoc& setLine(int line);
-  virtual SourceLoc& setCol(int col);
-  virtual SourceLoc& set(int line, int col);
+  int getLine() const;
+  int getCol() const;
+  SourceLoc& setLine(int line);
+  SourceLoc& setCol(int col);
+  SourceLoc& set(int line, int col);
 
-  virtual SourceLoc& invalidate();
-  virtual bool isValid() const;
+  SourceLoc& invalidate();
+  bool isValid() const;
 
-  virtual operator bool() const;
-  virtual bool operator==(const SourceLoc &other) const;
-  virtual bool operator!=(const SourceLoc &other) const;
-  virtual bool operator<(const SourceLoc &other) const;
-  virtual bool operator<=(const SourceLoc &other) const;
-  virtual bool operator>(const SourceLoc &other) const;
-  virtual bool operator>=(const SourceLoc &other) const;
+  operator bool() const;
+  bool operator==(const SourceLoc &other) const;
+  bool operator!=(const SourceLoc &other) const;
+  bool operator<(const SourceLoc &other) const;
+  bool operator<=(const SourceLoc &other) const;
+  bool operator>(const SourceLoc &other) const;
+  bool operator>=(const SourceLoc &other) const;
 
   friend std::ostream & operator<<(std::ostream &os, const SourceLoc &loc);
   friend llvm::raw_ostream & operator<<(llvm::raw_ostream &os, SourceLoc &loc);
