bed47abe388fa2ab43d2771bd36c20b9ade86942
George Karlos <gkarloscodes@gmail.com>
2020-09-19 03:42:07 +0200

    [nvvm] Added address spaces

--- include/kerma/NVVM/NVVM.h
+++ include/kerma/NVVM/NVVM.h
@@ -70,6 +70,51 @@ public:
   const std::string z = "llvm.nvvm.read.ptx.sreg.tid.z";
 } ThreadIdx;
 
+
+namespace AddressSpace {
+
+  namespace detail {
+    struct AddressSpaceImpl {
+    private:
+      const char *name_;
+    public:
+      constexpr AddressSpaceImpl(const char *name, int id) : name_(name), ID(id) {}
+      const int ID;
+      operator std::string() const { return name_;}
+      constexpr operator int() const { return ID; }
+      constexpr bool operator==(const AddressSpaceImpl& other) { return ID == other.ID; }
+      const std::string name() const { return name_; }
+    };
+  } // namespace detail
+
+  constexpr detail::AddressSpaceImpl Global("global", 1);
+  constexpr detail::AddressSpaceImpl Shared("shared", 3);
+  constexpr detail::AddressSpaceImpl Constant("constant", 4);
+  constexpr detail::AddressSpaceImpl Local("local", 5);
+  constexpr detail::AddressSpaceImpl Generic("generic", 0);
+  constexpr detail::AddressSpaceImpl Internal("internal",2);
+  constexpr detail::AddressSpaceImpl Unknown("unknown", -1);
+} // namespace AddressSpace
+
+inline const AddressSpace::detail::AddressSpaceImpl& getAddressSpaceWithId(int id) {
+  switch(id) {
+    case AddressSpace::Global:
+      return AddressSpace::Global;
+    case AddressSpace::Shared:
+      return AddressSpace::Shared;
+    case AddressSpace::Constant:
+      return AddressSpace::Constant;
+    case AddressSpace::Local:
+      return AddressSpace::Local;
+    case AddressSpace::Generic:
+      return AddressSpace::Generic;
+    case AddressSpace::Internal:
+      return AddressSpace::Internal;
+    default:
+      return AddressSpace::Unknown;
+  }
+}
+
 } // namespace nvvm
 } // namespace kerma
 
