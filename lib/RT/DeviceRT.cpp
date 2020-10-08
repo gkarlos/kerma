eed7ee5566ef7f6a53a40009a5d1740cd85c7049
George Karlos <gkarloscodes@gmail.com>
2020-10-08 21:03:39 +0200

    [RT] Add more RT functions and extend utilities

--- lib/RT/DeviceRT.cpp
+++ lib/RT/DeviceRT.cpp
@@ -1,6 +1,6 @@
 /// If this global is present and has this value, then the
 /// module is considered to be linked with the RT library.
-__device__ __constant__ unsigned int ___kerma_rt_linked___ = 0xFEEDC0DE;
+__device__ __constant__ unsigned int __kerma_rt_linked__ = 0xFEEDC0DE;
 
 /// Ultimately we want to minimize the complexity of each stub as
 /// much as possible. For instance to remove brances and let the
@@ -14,8 +14,28 @@ __device__ __constant__ unsigned int ___kerma_rt_linked___ = 0xFEEDC0DE;
 #define __kerma_mem_access_store__ 1
 #define __kerma_mem_access_atomic__ 2
 
+extern "C" __device__ void __kerma_rec_kernel(unsigned char id,
+                                             const char *name) {
+  if (blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0 &&
+      threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
+    printf("%%K.%d.%s\n", id, name);
+  }
+}
+
+extern "C" __device__ void __kerma_rec_base(unsigned char kernelid,
+                                      const char *symbol,
+                                      unsigned char addrspace,
+                                      unsigned long baseaddr) {
+  unsigned int linearBlockIdx =
+      blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+  if (linearBlockIdx == 0 && threadIdx.x == 0 && threadIdx.y == 0 &&
+      threadIdx.z == 0) {
+    printf("%%B.%u.<%d>%s@%lu\n", kernelid, addrspace, symbol, baseaddr);
+  }
+}
+
 /// Record all threads in a block
-extern "C" __device__ void __rec_access_b(__kerma_mem_access_type__ ty,
+extern "C" __device__ void __kerma_rec_access_b(__kerma_mem_access_type__ ty,
                                           unsigned int bid, unsigned int line,
                                           unsigned int col, const char *symbol,
                                           unsigned long loc) {
@@ -35,7 +55,7 @@ extern "C" __device__ void __rec_access_b(__kerma_mem_access_type__ ty,
 }
 
 /// Record threads in warp 0 of a block
-extern "C" __device__ void __rec_access_w(__kerma_mem_access_type__ ty,
+extern "C" __device__ void __kerma_rec_access_w(__kerma_mem_access_type__ ty,
                                           unsigned int bid, unsigned int line,
                                           unsigned int col, const char *symbol,
                                           unsigned long loc) {
@@ -56,7 +76,7 @@ extern "C" __device__ void __rec_access_w(__kerma_mem_access_type__ ty,
 }
 
 /// Record a specific thread in a block
-extern "C" __device__ void __rec_access_b_t(__kerma_mem_access_type__ ty,
+extern "C" __device__ void __kerma_rec_access_b_t(__kerma_mem_access_type__ ty,
                                             unsigned int bid, unsigned int tid,
                                             unsigned int line, unsigned int col,
                                             const char *symbol,
@@ -72,7 +92,7 @@ extern "C" __device__ void __rec_access_b_t(__kerma_mem_access_type__ ty,
       char c = ty == __kerma_mem_access_load__
                    ? 'L'
                    : (ty == __kerma_mem_access_store__ ? 'S' : 'A');
-      printf("%c(%u,%u,%u:%u):%s[%lu]\n", c, bid, tid, line, col, symbol, loc);
+      printf("!%c(%u,%u,%u:%u):%s[%lu]\n", c, bid, tid, line, col, symbol, loc);
     }
   }
 }
--- lib/RT/Util.cpp
+++ lib/RT/Util.cpp
@@ -2,27 +2,47 @@
 
 #include "llvm/IR/Type.h"
 #include "llvm/Support/raw_ostream.h"
+#include <algorithm>
 #include <llvm/ADT/APInt.h>
+#include <llvm/Demangle/Demangle.h>
 #include <llvm/IR/Constant.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/IR/DerivedTypes.h>
 #include <llvm/Support/Casting.h>
 
-namespace kerma {
 using namespace llvm;
+namespace kerma {
 
-const std::string KermaRTLinkedSymbol      = "___kerma_rt_linked___";
-const unsigned int KermaRTLinkSymbolValue  = 0xFEEDC0DE;
+const std::string KermaRTLinkedSymbol = "__kerma_rt_linked__";
+const unsigned int KermaRTLinkSymbolValue = 0xFEEDC0DE;
 const Type::TypeID KermaRTLinkSymbolTypeID = Type::IntegerTyID;
 
+static std::string KermaGlobalNamePrefix = "__kerma_sym";
+
+static std::vector<std::string> DeviceRTFunctions = {
+  "__kerma_rec_kernel",
+  "__kerma_rec_base",
+  "__kerma_rec_access_b",
+  "__kerma_rec_access_w",
+  "__kerma_rec_access_b_t" };
+
+static std::vector<std::string> DeviceRTFunctionSignatures = {
+  "__kerma_rec_kernel(unsigned int, char const*)",
+  "__kerma_rec_base(unsigned int, char const*, unsigned int, unsigned int)",
+  "__kerma_rec_access_b(unsigned int, unsigned int, unsigned int, unsigned int, char const*, unsigned int)",
+  "__kerma_rec_access_w(unsigned int, unsigned int, unsigned int, unsigned int, char const*, unsigned int)",
+  "__kerma_rec_access_b_t(unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, char const*, unsigned int)" };
+
 /// Check if libKermaRT is linked with a  module
-bool KermaRTLinked(const llvm::Module& M) {
-  auto& Globals = M.getGlobalList();
-  for ( auto& global : Globals) {
-    if ( global.getName() == KermaRTLinkedSymbol) {
-      if ( global.getInitializer()->getType()->getTypeID() == KermaRTLinkSymbolTypeID) {
-        if ( auto* ConstInt = llvm::dyn_cast<ConstantInt>(global.getInitializer())) {
-          if ( !(KermaRTLinkSymbolValue ^ ConstInt->getZExtValue())) {
+bool KermaRTLinked(const llvm::Module &M) {
+  auto &Globals = M.getGlobalList();
+  for (auto &global : Globals) {
+    if (global.getName() == KermaRTLinkedSymbol) {
+      if (global.getInitializer()->getType()->getTypeID() ==
+          KermaRTLinkSymbolTypeID) {
+        if (auto *ConstInt =
+                llvm::dyn_cast<ConstantInt>(global.getInitializer())) {
+          if (!(KermaRTLinkSymbolValue ^ ConstInt->getZExtValue())) {
             return true;
           }
         }
@@ -32,15 +52,30 @@ bool KermaRTLinked(const llvm::Module& M) {
   return false;
 }
 
+bool isDeviceRTFunction(std::string& FName) {
+  return std::find(DeviceRTFunctions.begin(),
+                   DeviceRTFunctions.end(),
+                   FName) != DeviceRTFunctions.end() ||
+         std::find(DeviceRTFunctionSignatures.begin(),
+                   DeviceRTFunctionSignatures.end(),
+                   FName) != DeviceRTFunctionSignatures.end();
+}
+
+bool isDeviceRTFunction(llvm::Function &F) {
+  auto name = demangle(F.getName().str());
+  return std::find(DeviceRTFunctionSignatures.begin(),
+                   DeviceRTFunctionSignatures.end(),
+                   name) != DeviceRTFunctionSignatures.end();
+}
 
 ///
 /// Exceptions
 ///
 
-KermaRTNotFoundError::KermaRTNotFoundError(const std::string& Msg) : std::runtime_error("KermaRT not found: " + Msg)
-{}
+KermaRTNotFoundError::KermaRTNotFoundError(const std::string &Msg)
+    : std::runtime_error("KermaRT not found: " + Msg) {}
 
-KermaRTIRParseError::KermaRTIRParseError(const std::string& Msg) : std::runtime_error("KermaRT not parsed: " + Msg)
-{}
+KermaRTIRParseError::KermaRTIRParseError(const std::string &Msg)
+    : std::runtime_error("KermaRT not parsed: " + Msg) {}
 
 } // namespace kerma
