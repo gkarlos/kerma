b1a171586133fbf6d3b66baa54647ca57e410675
George Karlos <gkarloscodes@gmail.com>
2020-10-21 12:05:36 +0200

    [DetectKernels] Small Fixes + colored output

--- lib/Analysis/DetectKernels.cpp
+++ lib/Analysis/DetectKernels.cpp
@@ -1,10 +1,21 @@
 #include "kerma/Analysis/DetectKernels.h"
+
+#include "kerma/NVVM/NVVMUtilities.h"
+
 #include "llvm/IR/Metadata.h"
 #include "llvm/Pass.h"
 #include "llvm/PassSupport.h"
 #include "llvm/Support/CommandLine.h"
+#include <llvm/ADT/SmallSet.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/IR/Module.h>
 #include <llvm/Support/Casting.h>
+#include <llvm/Support/FormatVariadic.h>
+#include <llvm/Support/WithColor.h>
+
 #include <memory>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 
@@ -18,14 +29,13 @@ DetectKernelsPass::DetectKernelsPass()
 : llvm::ModulePass(ID)
 {}
 
-std::vector<Function*> DetectKernelsPass::getKernels() {
-  std::vector<Function*> res(this->Kernels);
-  return res;
+const std::vector<Kernel>& DetectKernelsPass::getKernels() {
+  return Kernels;
 }
 
-void DetectKernelsPass::getKernels(std::vector<Function*> &Kernels) {
-  for ( auto *F : this->Kernels)
-    Kernels.push_back(F);
+void DetectKernelsPass::getKernels(std::vector<Kernel>& Kernels) {
+  for ( auto &Kernel : this->Kernels)
+    Kernels.push_back(Kernel);
 }
 
 bool DetectKernelsPass::runOnModule(llvm::Module &M) {
@@ -39,17 +49,19 @@ bool DetectKernelsPass::runOnModule(llvm::Module &M) {
           //  check if Arg 1 of the MDNode is the string kernel
           if ( MDString *MDStr = dyn_cast_or_null<MDString>(node->getOperand(1).get())) {
             if ( MDStr->getString() == "kernel")
-              this->Kernels.push_back(F);
+              Kernels.push_back( Kernel(F));
           }
         }
       }
     }
   }
-  return false;
-}
 
-bool DetectKernelsPass::isKernel(Function *F) {
-  return std::find(Kernels.begin(), Kernels.end(), F)  != Kernels.end();
+  WithColor(errs(), HighlightColor::Note) << '[';
+  WithColor(errs(), raw_ostream::Colors::GREEN) << formatv("{0,15}", "KernelDetector");
+  WithColor(errs(), HighlightColor::Note) << ']';
+  errs() << ' ' << Kernels.size() << " kernels\n";
+
+  return false;
 }
 
 /// This method is invoked when the pass is run in
@@ -59,7 +71,7 @@ void DetectKernelsPass::print(llvm::raw_ostream &O, const Module *M) const {
   if ( this->Kernels.size()) {
     O << ":\n";
     for ( int i = 0; i < this->Kernels.size(); ++i)
-      O << "  " << i << ". " << this->Kernels.at(i)->getName() << "\n";
+      O << "  " << i << ". " << this->Kernels.at(i).getDemangledName() << "\n";
   } else {
     O << "\n";
   }
@@ -76,25 +88,60 @@ createDetectKernelsPass() {
 
 namespace {
 
-static llvm::RegisterPass<kerma::DetectKernelsPass> RegisterDetectKernelsPass(/* pass arg  */    "kerma-detect-kernels",
-                                                                 /* pass name */    "Detect kernel functions",
-                                                                 /* modifies CFG */ false,
-                                                                 /* analysis pass*/ true);
+static llvm::RegisterPass<kerma::DetectKernelsPass> RegisterDetectKernelsPass(
+                                /* pass arg  */    "kerma-detect-kernels",
+                                /* pass name */    "Detect kernel functions",
+                                /* modifies CFG */ false,
+                                /* analysis pass*/ true);
 }
 
 
-bool isKerneFunction(const llvm::Function &F) {
-  NamedMDNode *MD = F.getParent()->getNamedMetadata("nvvm.annotations");
-  if(MD) {
-    for ( auto* MDNode : MD->operands()) {
-      if ( ValueAsMetadata *VAM = dyn_cast_or_null<ValueAsMetadata>(MDNode->getOperand(0).get()))
-        if ( Function *Fun = dyn_cast<Function>(VAM->getValue()))
-          if ( auto* MDStr = dyn_cast_or_null<MDString>(MDNode->getOperand(0).get()))
-            if ( MDStr->getString() == "kernel")
-              return Fun == &F;
+static std::map<const Module*, std::vector<Kernel>> KernelCache;
+
+static void findKernels(const llvm::Module &M) {
+  // First clear the cache
+  if ( auto Entry = KernelCache.find(&M); Entry != KernelCache.end())
+    Entry->second.clear();
+  else
+    KernelCache.insert(std::make_pair(&M, std::vector<Kernel>()));
+
+  std::vector<Kernel>& Kernels = KernelCache[&M];
+
+  if (NamedMDNode * NVVMMD = M.getNamedMetadata("nvvm.annotations")) {
+    for (const MDNode *MDNode : NVVMMD->operands()) {
+      for (const MDOperand &Op : MDNode->operands()) {
+        Metadata * MD = Op.get();
+        if ( ValueAsMetadata *VAMD = dyn_cast_or_null<ValueAsMetadata>(MD) )
+          if ( Function *F = dyn_cast<Function>(VAMD->getValue()) )
+            Kernels.push_back(Kernel(F));
+      }
     }
   }
-  return F.getCallingConv() == CallingConv::PTX_Kernel;
+}
+
+std::vector<Function*> getKernelFunctions(const Module &M, bool invalidateCacheEntry) {
+
+  if ( invalidateCacheEntry || KernelCache.find(&M) == KernelCache.end())
+    findKernels(M);
+
+  auto& Kernels = KernelCache[&M];
+
+  std::vector<Function*> Res;
+
+  for ( auto &Kernel : Kernels)
+    Res.push_back(Kernel.getFunction());
+
+  return Res;
+}
+
+const std::vector<Kernel> & getKernels(const Module &M, bool invalidateCacheEntry) {
+  if ( invalidateCacheEntry || KernelCache.find(&M) == KernelCache.end())
+    findKernels(M);
+  return KernelCache[&M];
+}
+
+bool isKerneFunction(const llvm::Function &F) {
+  return nvvm::isKernelFunction(F);
 }
 
 } // end namespace kerma
