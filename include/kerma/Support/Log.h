a8462925efd6489f343b330484dac9eb4d556b3e
George Karlos <gkarloscodes@gmail.com>
2020-11-01 21:34:17 +0100

    [Support] Wrappers for nlohmann/json and spdlog

---
+++ include/kerma/Support/Log.h
@@ -0,0 +1,15 @@
+#ifndef KERMA_SUPPORT_LOG_H
+#define KERMA_SUPPORT_LOG_H
+
+#include <spdlog/spdlog.h>
+
+namespace kerma {
+
+namespace Log = spdlog;
+
+#define LOG_SEP "================================================"\
+                "================================================"
+
+}
+
+#endif // KERMA_SUPPORT_LOG_H
