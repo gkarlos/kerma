f5d06ba0dcdc2309c85f8367a06ed482014ecbc2
George Karlos <gkarloscodes@gmail.com>
2020-11-04 13:58:02 +0100

    [Support] Remove some functions from Version.cpp to fix compilation

--- lib/Support/Version.cpp
+++ lib/Support/Version.cpp
@@ -14,37 +14,37 @@
 
 namespace kerma {
 
-std::string getVersionMajor() {
-#ifdef KERMA_VERSION_MAJOR
-  return KERMA_VERSION_MAJOR;
-#else
-  return "";
-#endif
-}
+// std::string getVersionMajor() {
+// #ifdef KERMA_VERSION_MAJOR
+//   return KERMA_VERSION_MAJOR;
+// #else
+//   return "";
+// #endif
+// }
 
-std::string getVersionMinor() {
-#ifdef KERMA_VERSION_MINOR
-  return KERMA_VERSION_MINOR;
-#else
-  return "";
-#endif
-}
+// std::string getVersionMinor() {
+// #ifdef KERMA_VERSION_MINOR
+//   return KERMA_VERSION_MINOR;
+// #else
+//   return "";
+// #endif
+// }
 
-std::string getVersionPatch() {
-#ifdef KERMA_VERSION_PATCH
-  return KERMA_VERSION_PATCH;
-#else
-  return "";
-#endif
-}
+// std::string getVersionPatch() {
+// #ifdef KERMA_VERSION_PATCH
+//   return KERMA_VERSION_PATCH;
+// #else
+//   return "";
+// #endif
+// }
 
-std::string getVersion() {
-  auto minor = getVersionMinor();
-  auto patch = getVersionPatch();
-  return getVersionMajor()
-    + (minor.size() > 0 ? "." + minor : "")
-    + (patch.size() > 0 ? "." + patch : "");
-}
+// std::string getVersion() {
+//   auto minor = getVersionMinor();
+//   auto patch = getVersionPatch();
+//   return getVersionMajor()
+//     + (minor.size() > 0 ? "." + minor : "")
+//     + (patch.size() > 0 ? "." + patch : "");
+// }
 
 std::string getLLVMVersion() {
   return std::to_string(LLVM_VERSION_MAJOR) + "." +
