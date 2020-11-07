8511ce438e7a0c1aaddbfa19ffbfc432131844c5
George Karlos <gkarloscodes@gmail.com>
2020-11-07 03:53:34 +0100

    [SourceInfo][Builder] Better error throwing

--- lib/SourceInfo/SourceInfoBuilder.cpp
+++ lib/SourceInfo/SourceInfoBuilder.cpp
@@ -7,7 +7,6 @@
 #include <boost/filesystem.hpp>
 #include <memory>
 
-
 // #include <clang/Frontend/TextDiagnosticPrinter.h>
 
 namespace fs = boost::filesystem;
@@ -16,35 +15,37 @@ namespace kerma {
 using namespace clang;
 using namespace clang::tooling;
 
-SourceInfoBuilder::SourceInfoBuilder( const std::string &SourcePath,
-                                      clang::tooling::CompilationDatabase *DB)
-{
-  if ( !fs::exists(SourcePath))
-    throw SourcePath + " does not exist";
-  if ( fs::is_directory(SourcePath))
-    throw SourcePath + " is a directory";
+SourceInfoBuilder::SourceInfoBuilder(const std::string &SourcePath,
+                                     clang::tooling::CompilationDatabase *DB) {
+  if (!fs::exists(SourcePath))
+    throw std::runtime_error(SourcePath + " does not exist");
+  if (fs::is_directory(SourcePath))
+    throw std::runtime_error(SourcePath + " is a directory");
 
-  if ( !DB) {
+  if (!DB) {
     std::string Err = "Could not read compile_commands.json";
-    auto db = CompilationDatabase::autoDetectFromSource(SourcePath,Err);
-    if ( !db) throw Err;
+    auto db = CompilationDatabase::autoDetectFromSource(SourcePath, Err);
+    if (!db)
+      throw Err;
     CompileDB = db.release();
   }
 
   auto RealSourcePath = get_realpath(SourcePath);
-  if ( RealSourcePath.empty())
-    throw std::runtime_error(std::string("Error getting real path for ") + SourcePath);
+  if (RealSourcePath.empty())
+    throw std::runtime_error(std::string("Error getting real path for ") +
+                             SourcePath);
   /// The requested file must have an entry in the Compilation Database
-  if ( auto DBFiles = CompileDB->getAllFiles();
-       std::find(DBFiles.begin(), DBFiles.end(), RealSourcePath) == DBFiles.end())
-    throw std::runtime_error(RealSourcePath + " not found in CompilationDatabase");
+  if (auto DBFiles = CompileDB->getAllFiles();
+      std::find(DBFiles.begin(), DBFiles.end(), RealSourcePath) ==
+      DBFiles.end())
+    throw std::runtime_error(RealSourcePath +
+                             " not found in CompilationDatabase");
 
   // this->SourcePath = RealSourcePath;
   SourcePaths.push_back(RealSourcePath);
   Tool = std::make_unique<clang::tooling::ClangTool>(*CompileDB, SourcePaths);
   // FunctionRanges = std::make_unique<FunctionRangeActionFactory>();
 }
-
 
 SourceInfo SourceInfoBuilder::getSourceInfo() {
   Tool->run(&ActionFactory);
