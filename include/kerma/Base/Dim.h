54a53172d2b0e9eca169537b17e0aa961517aee0
George Karlos <gkarloscodes@gmail.com>
2020-11-08 18:52:14 +0100

    [Base][Dim] Make z,y,x instead of x,y,z and some other small fixes

--- include/kerma/Base/Dim.h
+++ include/kerma/Base/Dim.h
@@ -10,128 +10,129 @@ namespace kerma {
 class Index;
 
 /// This class represents dimensionality info
-/// It is used to describe the size of 1,2 or 3-dimensional objects
-/// Base class for all dim-like classes (e.g CuDim)
+/// It is used to describe sizes of objects,
+/// in 1, 2 or 3 dimensions.
+/// Semantics are similar to CUDA dim3
 class Dim {
 public:
-  Dim(unsigned int x=1, unsigned int y=1, unsigned int z=1);
+  /// Default constructor. Creates unit dim (0,0,1)
+  Dim();
+  /// If x is zero the Dim is unknown
+  Dim(unsigned x);
+  /// If any of y,z is zero then all become
+  /// zero and the Dim is unknown
+  Dim(unsigned y, unsigned x);
+  /// If any of x,y,z is zero then all become
+  /// zero and the Dim is unknown
+  Dim(unsigned z, unsigned y, unsigned x);
   Dim(const Dim &other);
   Dim(const Dim &&other);
-  virtual ~Dim()=default;
+  virtual ~Dim() = default;
 
 public:
-  //===-------
-  // Equality comparison operators
-  //===-------
   virtual bool operator==(const Dim &other) const;
   virtual bool operator!=(const Dim &other) const;
-  
-  //===-------
-  // Conversion operators
-  //===-------
   virtual operator unsigned long long() const;
+  virtual Dim &operator=(const Dim &other);
 
-  //===-------
-  // Other operators
-  //===-------
-  virtual Dim& operator=(const Dim &other);
+  /// Index 0 refers to the x-dimension, 1 to the y-dimension and 2 to the
+  /// z-dimension.
+  /// @throw std::out_of_range {}
+  virtual unsigned operator[](unsigned int idx) const;
 
-  // Index 0 refers to the x-dimension, 1 to the y-dimension and 2 to the z-dimension.
-  // @throw std::out_of_range {}
-  virtual unsigned int operator[](unsigned int idx) const;
-  virtual unsigned int operator()(unsigned int idx) const;
+  /// Index 0 refers to the x-dimension, 1 to the y-dimension and 2 to the
+  /// z-dimension.
+  /// @throw std::out_of_range {}
+  virtual unsigned operator()(unsigned int idx) const;
 
-  //
-  friend llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const Dim& dim);
+  ///
+  friend llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const Dim &dim);
 
-  //
-  friend std::ostream& operator<<(std::ostream& os, const Dim& dim);
+  ///
+  friend std::ostream &operator<<(std::ostream &os, const Dim &dim);
 
 public:
-
-  // Check if unknown
+  /// Check if unknown
   bool isUnknown() const;
 
-  // Retrieve the size of this dim
-  unsigned long long size() const; 
+  /// Retrieve the size of this dim
+  unsigned long long size() const;
 
-  // Check if the dim is 1-dimensional, 
-  // i.e spans only in the x-dimension
-  bool is1D() const; 
+  /// Check if the dim is 1-dimensional,
+  /// i.e spans only in the x-dimension
+  bool is1D() const;
 
-  // Check if the dim is 2-dimensional,
-  // i.e spans in the y-dimension (x-dimension may still be 1)
+  /// Check if the dim is 2-dimensional,
+  /// i.e spans in the y-dimension (x-dimension may still be 1)
   bool is2D() const;
 
-  // Check if the dim is 3-dimensional,
-  // i.e spans into the z-dimensions (x and y dimensions may still be 1)
+  /// Check if the dim is 3-dimensional,
+  /// i.e spans into the z-dimensions (x and y dimensions may still be 1)
   bool is3D() const;
 
-  // Check if the dim grows at most in 1 dimension
-  // The difference compared to is1D() is that y and z 
-  // are also considered. Example:
-  //
-  // Dim dimA(1);   // is1D and isEffective1D
-  // Dim dimB(1,2); // is2D and isEffective1D
-  // Dim dimC(1,1,2); // is3D and isEffective1D
+  /// Check if the dim grows at most in 1 dimension
+  /// The difference compared to is1D() is that y and z
+  /// are also considered. Example:
+  /// \code
+  /// Dim dimA(1);     // is1D and isEffective1D
+  /// Dim dimB(1,2);   // is2D and isEffective1D
+  /// Dim dimC(1,1,2); // is3D and isEffective1D
+  /// \endcode
   bool isEffective1D() const;
 
-  // Check if the dim grows exactly in 2 dimensions
-  // The difference compared to issD() is that any 
-  // combination is considered. Example:
-  //
-  // Dim dimA(1,2,1); // is2D and isEffective1D
-  // Dim dimB(1,2,2); // is3D and isEffective2D
-  // Dim dimC(3,1,3); // is3D and isEffective2D
+  /// Check if the dim grows exactly in 2 dimensions
+  /// The difference compared to is2D() is that any
+  /// combination is considered. Example:
+  /// \code
+  /// Dim dimA(1,2,1); // is2D and isEffective1D
+  /// Dim dimB(1,2,2); // is3D and isEffective2D
+  /// Dim dimC(3,1,3); // is3D and isEffective2D
+  /// \endcode
   bool isEffective2D() const;
 
-  // Check if an index is valid index for this dim
-  bool hasIndex(const Index& idx) const;
+  /// Check if an index is valid index for this dim
+  bool hasIndex(const Index &idx) const;
   bool hasIndex(unsigned long long linearIdx) const;
 
-  // Check if a linear index is valid index for this dim
-  // The index is first delinearized based on the dim's
-  // values.
+  /// Check if a linear index is valid index for this dim
+  /// The index is first delinearized based on the dim's
+  /// values.
   bool hasLinearIndex(unsigned long long idx) const;
 
-  // Get the minimum index for this dim
-  // (0,0,0) unless overriden
+  /// Get the minimum index for this dim
+  /// (0,0,0) unless overriden
   virtual Index getMinIndex() const;
 
-  // Get the maximum index for this dim (inclusive).
-  // (Z-1,Y-1,X-1) unless overriden
+  /// Get the maximum index for this dim (inclusive).
+  /// (Z-1,Y-1,X-1) unless overriden
   virtual Index getMaxIndex() const;
 
-  // Get the mimimum linear index for this dim
-  // 0 unless overriden
+  /// Get the mimimum linear index for this dim
+  /// 0 unless overriden
   virtual unsigned long long getMinLinearIndex() const;
 
-  // Get the maximum linear index for this dim
+  /// Get the maximum linear index for this dim
   virtual unsigned long long getMaxLinearIndex() const;
 
   virtual unsigned int cols() const;
   virtual unsigned int rows() const;
   virtual unsigned int layers() const;
-  
+
   virtual unsigned int width() const;
   virtual unsigned int height() const;
   virtual unsigned int depth() const;
-  
-public:
-  unsigned int x,y,z;
 
 public:
-  static Dim of(unsigned int x, unsigned int y, unsigned int z);
+  unsigned int x, y, z;
 
-  /// Special DIM used to indicated erroneous dim or 
-  /// unknown dim
+public:
+  /// Special DIM used to indicated erroneous 
+  /// dim or  unknown dim
   static const Dim None;
-  
+
   /// The unit dim
   static const Dim Unit;
 
-  ///
-  static const Dim& Linear1;
   static const Dim Linear2;
   static const Dim Linear4;
   static const Dim Linear8;
@@ -143,7 +144,6 @@ public:
   static const Dim Linear512;
   static const Dim Linear1024;
 
-  static const Dim& Square1;
   static const Dim Square2;
   static const Dim Square4;
   static const Dim Square8;
@@ -155,7 +155,6 @@ public:
   static const Dim Square512;
   static const Dim Square1024;
 
-  static const Dim& Cube1;
   static const Dim Cube2;
   static const Dim Cube4;
   static const Dim Cube8;
--- lib/Base/Dim.cpp
+++ lib/Base/Dim.cpp
@@ -2,26 +2,29 @@
 #include "kerma/Base/Index.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <algorithm> 
+#include <algorithm>
+#include <iostream>
 #include <memory>
 #include <ostream>
 #include <stdexcept>
-#include <iostream>
 #include <string>
 
 namespace kerma {
 
-Dim::Dim(unsigned int x, unsigned int y, unsigned int z)
-: x(x), y(y), z(z)
-{}
+Dim::Dim() : Dim(1) {}
+Dim::Dim(unsigned x) : Dim(x? 1 : 0, x) {}
+Dim::Dim(unsigned y, unsigned x) : Dim(y & x ? 1 : 0, y, x) {}
+Dim::Dim(unsigned x, unsigned y, unsigned z) : x(x), y(y), z(z) {
+  if ( !x || !y || !z) {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+  }
+}
 
-Dim::Dim(const Dim &other) 
-: x(other.x), y(other.y), z(other.z)
-{}
+Dim::Dim(const Dim &other) : x(other.x), y(other.y), z(other.z) {}
 
-Dim::Dim(const Dim &&other) 
-: x(other.x), y(other.y), z(other.z)
-{}
+Dim::Dim(const Dim &&other) : x(other.x), y(other.y), z(other.z) {}
 
 //===-------
 // Equality comparison operators
@@ -31,23 +34,19 @@ bool Dim::operator==(const Dim &other) const {
   return this->x == other.x && this->y == other.y && this->z == other.z;
 }
 
-bool Dim::operator!=(const Dim &other) const {
-  return !(*this == other);
-}
+bool Dim::operator!=(const Dim &other) const { return !(*this == other); }
 
 //===-------
 // Conversion operators
 //===-------
-Dim::operator unsigned long long() const {
-  return this->size();
-}
+Dim::operator unsigned long long() const { return this->size(); }
 
 //===-------
 // Other operators
 //===-------
 
-Dim& Dim::operator=(const Dim &other) {
-  if ( this != &other) {
+Dim &Dim::operator=(const Dim &other) {
+  if (this != &other) {
     x = other.x;
     y = other.y;
     z = other.z;
@@ -56,74 +55,76 @@ Dim& Dim::operator=(const Dim &other) {
 }
 
 unsigned int Dim::operator[](unsigned int idx) const {
-  switch ( idx) {
-    case 0: return this->x;
-    case 1: return this->y;
-    case 2: return this->z;
-    default:
-      throw std::out_of_range(std::string("Index ") + std::to_string(idx) + " is out of range");
+  switch (idx) {
+  case 0:
+    return this->x;
+  case 1:
+    return this->y;
+  case 2:
+    return this->z;
+  default:
+    throw std::out_of_range(std::string("Index ") + std::to_string(idx) +
+                            " is out of range");
   }
 }
 
 unsigned int Dim::operator()(unsigned int idx) const {
-  switch ( idx) {
-    case 0: return this->x;
-    case 1: return this->y;
-    case 2: return this->z;
-    default:
-      throw std::out_of_range(std::string("Index ") + std::to_string(idx) + " is out of range");
+  switch (idx) {
+  case 0:
+    return this->x;
+  case 1:
+    return this->y;
+  case 2:
+    return this->z;
+  default:
+    throw std::out_of_range(std::string("Index ") + std::to_string(idx) +
+                            " is out of range");
   }
 }
 
-std::ostream& operator<<(std::ostream& os, const Dim& dim) {
-  if ( dim.is1D())
+std::ostream &operator<<(std::ostream &os, const Dim &dim) {
+  if (dim.is1D())
     os << "<" << dim.x << ">";
-  else if ( dim.is2D())
-    os << "<" << dim.x << "," << dim.y << ">";
+  else if (dim.is2D())
+    os << "<" << dim.y << "," << dim.x << ">";
   else
-    os << "<" << dim.x << "," << dim.y << "," << dim.z << ">";
+    os << "<" << dim.z << "," << dim.y << "," << dim.x << ">";
   return os;
 }
 
-llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const Dim& dim) {
-  os << "<" << dim.x << "," << dim.y << "," << dim.z << ">";
+llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const Dim &dim) {
+  if (dim.is1D())
+    os << "<" << dim.x << ">";
+  else if (dim.is2D())
+    os << "<" << dim.y << "," << dim.x << ">";
+  else
+    os << "<" << dim.z << "," << dim.y << "," << dim.x << ">";
   return os;
 }
 
-bool Dim::isUnknown() const {
-  return !*this;
-}
+bool Dim::isUnknown() const { return !*this; }
 
-unsigned long long Dim::size() const {
-  return this->x * this->y *this->z;
-}
+unsigned long long Dim::size() const { return this->x * this->y * this->z; }
 
-bool Dim::is1D() const {
-  return this->y == 1 && this->z == 1;
-}
+bool Dim::is1D() const { return this->y == 1; }
 
-bool Dim::is2D() const {
-  return this->y > 1 && this->z  == 1;
-}
+bool Dim::is2D() const { return this->y > 1 && this->z == 1; }
 
-bool Dim::is3D() const {
-  return this->z > 1;
-}
+bool Dim::is3D() const { return this->z > 1; }
 
 bool Dim::isEffective1D() const {
-  return this->is1D()
-    ||  (this->x == 1 && this->y == 1 && this->z  > 1)
-    ||  (this->x == 1 && this->y  > 1 && this->z == 1)
-    ||  (this->x  > 1 && this->y == 1 && this->z == 1);
+  return this->is1D() || (this->x == 1 && this->y == 1 && this->z > 1) ||
+         (this->x == 1 && this->y > 1 && this->z == 1) ||
+         (this->x > 1 && this->y == 1 && this->z == 1);
 }
 
 bool Dim::isEffective2D() const {
-  return (this->x > 1 && this->y > 1 && this->z == 1)
-      || (this->x > 1 && this->z > 1 && this->y == 1)
-      || (this->y > 1 && this->z > 1 && this->x == 1);
+  return (this->x > 1 && this->y > 1 && this->z == 1) ||
+         (this->x > 1 && this->z > 1 && this->y == 1) ||
+         (this->y > 1 && this->z > 1 && this->x == 1);
 }
 
-bool Dim::hasIndex(const Index& idx) const {
+bool Dim::hasIndex(const Index &idx) const {
   return idx.x < this->x && idx.y < this->y && idx.z < this->z;
 }
 
@@ -141,51 +142,31 @@ Index Dim::getMinIndex() const {
 }
 
 Index Dim::getMaxIndex() const {
-  if ( this->isUnknown())
-    return Index(0,0,0);
-  return Index(this->x - 1, this->y - 1, this->z - 1);
+  if (this->isUnknown())
+    return Index(0, 0, 0);
+  return Index(this->z - 1, this->y - 1, this->x - 1);
 }
 
-unsigned long long Dim::getMinLinearIndex() const {
-  return 0;
-}
+unsigned long long Dim::getMinLinearIndex() const { return 0; }
 
-unsigned long long Dim::getMaxLinearIndex() const {
-  return this->size() - 1;
-}
+unsigned long long Dim::getMaxLinearIndex() const { return this->size() - 1; }
 
-unsigned int Dim::cols() const { 
-  return this->x;
-}
+unsigned int Dim::cols() const { return this->x; }
 
-unsigned int Dim::rows() const {
-  return this->y;
-}
+unsigned int Dim::rows() const { return this->y; }
 
-unsigned int Dim::layers() const {
-  return this->z;
-}
+unsigned int Dim::layers() const { return this->z; }
 
-unsigned int Dim::width() const {
-  return this->x;
-}
+unsigned int Dim::width() const { return this->x; }
 
-unsigned int Dim::height() const {
-  return this->y;
-}
+unsigned int Dim::height() const { return this->y; }
 
-unsigned int Dim::depth() const {
-  return this->z;
-}
+unsigned int Dim::depth() const { return this->z; }
 
-Dim Dim::of(unsigned int x, unsigned int y, unsigned int z) {
-  return Dim(x, y, z);
-}
 
-const Dim Dim::None(0,0,0);
+const Dim Dim::None(0);
 const Dim Dim::Unit = Dim();
 
-const Dim& Dim::Linear1 = Dim::Unit;
 const Dim Dim::Linear2(2);
 const Dim Dim::Linear4(4);
 const Dim Dim::Linear8(8);
@@ -197,27 +178,25 @@ const Dim Dim::Linear256(256);
 const Dim Dim::Linear512(512);
 const Dim Dim::Linear1024(1024);
 
-const Dim& Dim::Square1 = Dim::Unit;
-const Dim Dim::Square2(2,2);
-const Dim Dim::Square4(4,4);
-const Dim Dim::Square8(8,8);
-const Dim Dim::Square16(16,16);
-const Dim Dim::Square32(32,32);
-const Dim Dim::Square64(64,64);
-const Dim Dim::Square128(128,128);
-const Dim Dim::Square256(256,256);
-const Dim Dim::Square512(512,512);
-const Dim Dim::Square1024(1024,1024);
+const Dim Dim::Square2(2, 2);
+const Dim Dim::Square4(4, 4);
+const Dim Dim::Square8(8, 8);
+const Dim Dim::Square16(16, 16);
+const Dim Dim::Square32(32, 32);
+const Dim Dim::Square64(64, 64);
+const Dim Dim::Square128(128, 128);
+const Dim Dim::Square256(256, 256);
+const Dim Dim::Square512(512, 512);
+const Dim Dim::Square1024(1024, 1024);
 
-const Dim& Dim::Cube1 = Dim::Unit;
-const Dim Dim::Cube2(2,2,2);
-const Dim Dim::Cube4(4,4,4);
-const Dim Dim::Cube8(8,8,8);
-const Dim Dim::Cube16(16,16,16);
-const Dim Dim::Cube32(32,32,32);
-const Dim Dim::Cube64(64,64,64);
-const Dim Dim::Cube128(128,128,128);
-const Dim Dim::Cube256(256,256,256);
-const Dim Dim::Cube512(512,512,512);
-const Dim Dim::Cube1024(1024,1024,1024);
+const Dim Dim::Cube2(2, 2, 2);
+const Dim Dim::Cube4(4, 4, 4);
+const Dim Dim::Cube8(8, 8, 8);
+const Dim Dim::Cube16(16, 16, 16);
+const Dim Dim::Cube32(32, 32, 32);
+const Dim Dim::Cube64(64, 64, 64);
+const Dim Dim::Cube128(128, 128, 128);
+const Dim Dim::Cube256(256, 256, 256);
+const Dim Dim::Cube512(512, 512, 512);
+const Dim Dim::Cube1024(1024, 1024, 1024);
 } // namespace kerma
