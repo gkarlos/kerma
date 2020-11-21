dbecdef2c4f7f85028638ed1be63557543594316
George Karlos <gkarloscodes@gmail.com>
2020-11-21 20:44:46 +0100

    [RT] Make __kerma_rec_kernel also record grid dims

--- lib/RT/DeviceRT.cpp
+++ lib/RT/DeviceRT.cpp
@@ -20,7 +20,8 @@ __device__ __constant__ unsigned int __kerma_rt_linked__ = 0xFEEDC0DE;
 /// is to just an IRBuilder to insert the relevant code.
 /// By having this function however we let codegen do that for us,
 /// so we do not have to insert extra code in the Instrumenter.
-extern "C" __device__ bool __kerma_trace_status(bool *stop_tracing, unsigned int kernel_id) {
+extern "C" __device__ bool __kerma_trace_status(bool *stop_tracing,
+                                                unsigned int kernel_id) {
   bool status = stop_tracing[kernel_id];
   return status;
 }
@@ -30,33 +31,32 @@ extern "C" __device__ bool __kerma_trace_status(bool *stop_tracing, unsigned int
 ///
 /// Similarly to __kerma_trace_status we put this function here to
 /// minimize the Instrumenter code.
-extern "C" __device__ void __kerma_stop_tracing(bool *stop_tracing, unsigned int kernel_id) {
+extern "C" __device__ void __kerma_stop_tracing(bool *stop_tracing,
+                                                unsigned int kernel_id) {
   __syncthreads();
-  if ( threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0)
+  if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0)
     stop_tracing[kernel_id] = true;
 }
 
-
-
 ///
-extern "C" __device__ void __kerma_rec_kernel(bool stop_tracing,
-                                              unsigned char id,
-                                              const char *name) {
-  if ( stop_tracing) return;
+extern "C" __device__ void
+__kerma_rec_kernel(bool stop_tracing, unsigned char id, const char *name) {
+  if (stop_tracing)
+    return;
 
   if (blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0 &&
       threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) {
-    printf("%%K.%d.%s\n", id, name);
+    printf("%%K.%d.%s<%u,%u,%u><%u,%u,%u>\n", id, name, gridDim.z, gridDim.y,
+           gridDim.x, blockDim.z, blockDim.y, blockDim.x);
   }
 }
 
-extern "C" __device__ void __kerma_rec_base(bool stop_tracing,
-                                            unsigned char kernelid,
-                                            const char *symbol,
-                                            unsigned char addrspace,
-                                            unsigned long baseaddr) {
+extern "C" __device__ void
+__kerma_rec_base(bool stop_tracing, unsigned char kernelid, const char *symbol,
+                 unsigned char addrspace, unsigned long baseaddr) {
 
-  if ( stop_tracing) return;
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
       blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
@@ -66,13 +66,13 @@ extern "C" __device__ void __kerma_rec_base(bool stop_tracing,
   }
 }
 
-extern "C" __device__ void __kerma_rec_access_b(bool stop_tracing,
-                                                __kerma_mem_access_type__ ty,
-                                                unsigned int bid,
-                                                unsigned int line, unsigned int col,
-                                                const char *name,
-                                                unsigned long offset, unsigned int size) {
-  if ( stop_tracing) return;
+extern "C" __device__ void
+__kerma_rec_access_b(bool stop_tracing, __kerma_mem_access_type__ ty,
+                     unsigned int bid, unsigned int line, unsigned int col,
+                     const char *name, unsigned long offset,
+                     unsigned int size) {
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
       blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
@@ -81,31 +81,25 @@ extern "C" __device__ void __kerma_rec_access_b(bool stop_tracing,
     unsigned int linearLocalThreadIdx = threadIdx.z * blockDim.x * blockDim.y +
                                         threadIdx.y * blockDim.x + threadIdx.x;
 
-    printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty,
-                                            bid,
-                                            linearLocalThreadIdx,
-                                            line,
-                                            col,
-                                            name,
-                                            offset,
-                                            size);
+    printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty, bid, linearLocalThreadIdx, line,
+           col, name, offset, size);
   }
 }
 
 /// Record all threads in a specific warp in a specific block
 /// The warp id must exist otherwise threads will perform
 /// unecessary work.
-extern "C" __device__ void __kerma_rec_access_w(bool stop_tracing,
-                                                __kerma_mem_access_type__ ty,
-                                                unsigned int bid,  unsigned int wid,
-                                                unsigned int line, unsigned int col,
-                                                const char *name, unsigned long offset,
-                                                unsigned int size) {
+extern "C" __device__ void
+__kerma_rec_access_w(bool stop_tracing, __kerma_mem_access_type__ ty,
+                     unsigned int bid, unsigned int wid, unsigned int line,
+                     unsigned int col, const char *name, unsigned long offset,
+                     unsigned int size) {
 
-  if ( stop_tracing) return;
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
-    blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+      blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
 
   if (linearBlockIdx == bid) {
     unsigned int linearLocalThreadIdx = threadIdx.z * blockDim.x * blockDim.y +
@@ -113,28 +107,22 @@ extern "C" __device__ void __kerma_rec_access_w(bool stop_tracing,
 
     unsigned int warpid = linearLocalThreadIdx / 32;
 
-    if ( warpid == wid) {
-      printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty,
-                                              bid,
-                                              linearLocalThreadIdx,
-                                              line,
-                                              col,
-                                              name,
-                                              offset,
-                                              size);
+    if (warpid == wid) {
+      printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty, bid, linearLocalThreadIdx,
+             line, col, name, offset, size);
     }
   }
 }
 
 /// Record a specific thread in a block
-extern "C" __device__ void __kerma_rec_access_t(bool stop_tracing,
-                                                __kerma_mem_access_type__ ty,
-                                                unsigned int bid, unsigned int tid,
-                                                unsigned int line, unsigned int col,
-                                                const char *name, unsigned long offset,
-                                                unsigned int size) {
+extern "C" __device__ void
+__kerma_rec_access_t(bool stop_tracing, __kerma_mem_access_type__ ty,
+                     unsigned int bid, unsigned int tid, unsigned int line,
+                     unsigned int col, const char *name, unsigned long offset,
+                     unsigned int size) {
 
-  if ( stop_tracing) return;
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
       blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
@@ -144,14 +132,8 @@ extern "C" __device__ void __kerma_rec_access_t(bool stop_tracing,
                                         threadIdx.y * blockDim.x + threadIdx.x;
 
     if (linearLocalThreadIdx == tid)
-      printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty,
-                                              bid,
-                                              tid,
-                                              line,
-                                              col,
-                                              name,
-                                              offset,
-                                              size);
+      printf("!%c(%u,%u,%u:%u):%s[%lu]:%u\n", ty, bid, tid, line, col, name,
+             offset, size);
   }
 }
 
@@ -164,56 +146,46 @@ extern "C" __device__ void __kerma_rec_access_t(bool stop_tracing,
 // Note that the record does not include address space info. The address space
 // for a (non-local) array is included in the %B records
 
-
 /// Record a copy for all threads in a block
-extern "C" __device__ void __kerma_rec_copy_b(bool stop_tracing,
-                                              unsigned int bid,
-                                              unsigned int line, unsigned int col,
-                                              const char *sname, unsigned long soffset,
-                                              const char *dname, unsigned long doffset,
-                                              unsigned int size) {
-  if ( stop_tracing) return;
+extern "C" __device__ void
+__kerma_rec_copy_b(bool stop_tracing, unsigned int bid, unsigned int line,
+                   unsigned int col, const char *sname, unsigned long soffset,
+                   const char *dname, unsigned long doffset,
+                   unsigned int size) {
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
-    blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+      blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
 
   if (linearBlockIdx == bid) {
     unsigned int linearLocalThreadIdx = threadIdx.z * blockDim.x * blockDim.y +
                                         threadIdx.y * blockDim.x + threadIdx.x;
 
-    if ( sname && dname) {
+    if (sname && dname) {
       printf("!C(%u,%u,%u:%u):%s[%lu]>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                     line, col,
-                                                     sname, soffset,
-                                                     dname, doffset,
-                                                     size);
-    }
-    else if ( sname) {
+             line, col, sname, soffset, dname, doffset, size);
+    } else if (sname) {
       printf("!C(%u,%u,%u:%u):%s[%lu]>loc:%u\n", bid, linearLocalThreadIdx,
-                                                 line, col,
-                                                 sname, soffset,
-                                                 size);
-    }
-    else {
+             line, col, sname, soffset, size);
+    } else {
       printf("!C(%u,%u,%u:%u):loc>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                 line, col,
-                                                 dname, doffset,
-                                                 size);
+             line, col, dname, doffset, size);
     }
   }
 }
 
 /// Record a copy for all threads in a specific warp in a specific block
-extern "C" __device__ void __kerma_rec_copy_w(bool stop_tracing,
-                                              unsigned int bid, unsigned int wid,
-                                              unsigned int line, unsigned int col,
-                                              const char *sname, unsigned long soffset,
-                                              const char *dname, unsigned long doffset,
-                                              unsigned int size) {
-  if ( stop_tracing) return;
+extern "C" __device__ void
+__kerma_rec_copy_w(bool stop_tracing, unsigned int bid, unsigned int wid,
+                   unsigned int line, unsigned int col, const char *sname,
+                   unsigned long soffset, const char *dname,
+                   unsigned long doffset, unsigned int size) {
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
-    blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+      blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
 
   if (linearBlockIdx == bid) {
     unsigned int linearLocalThreadIdx = threadIdx.z * blockDim.x * blockDim.y +
@@ -221,65 +193,49 @@ extern "C" __device__ void __kerma_rec_copy_w(bool stop_tracing,
 
     unsigned int warpid = linearLocalThreadIdx / 32;
 
-    if ( warpid == wid) {
-      if ( sname && dname) {
-        printf("!C(%u,%u,%u:%u):%s[%lu]>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                       line, col,
-                                                       sname, soffset,
-                                                       dname, doffset,
-                                                       size);
-      }
-      else if ( sname) {
+    if (warpid == wid) {
+      if (sname && dname) {
+        printf("!C(%u,%u,%u:%u):%s[%lu]>%s[%lu]:%u\n", bid,
+               linearLocalThreadIdx, line, col, sname, soffset, dname, doffset,
+               size);
+      } else if (sname) {
         printf("!C(%u,%u,%u:%u):%s[%lu]>loc:%u\n", bid, linearLocalThreadIdx,
-                                                   line, col,
-                                                   sname, soffset,
-                                                   size);
-      }
-      else {
+               line, col, sname, soffset, size);
+      } else {
         printf("!C(%u,%u,%u:%u):loc>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                   line, col,
-                                                   dname, doffset,
-                                                   size);
+               line, col, dname, doffset, size);
       }
     }
   }
 }
 
 /// Record a copy for all threads in a specific warp in a specific block
-extern "C" __device__ void __kerma_rec_copy_t(bool stop_tracing,
-                                              unsigned int bid, unsigned int tid,
-                                              unsigned int line, unsigned int col,
-                                              const char *sname, unsigned long soffset,
-                                              const char *dname, unsigned long doffset,
-                                              unsigned int size) {
-  if ( stop_tracing) return;
+extern "C" __device__ void
+__kerma_rec_copy_t(bool stop_tracing, unsigned int bid, unsigned int tid,
+                   unsigned int line, unsigned int col, const char *sname,
+                   unsigned long soffset, const char *dname,
+                   unsigned long doffset, unsigned int size) {
+  if (stop_tracing)
+    return;
 
   unsigned int linearBlockIdx =
-    blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
+      blockIdx.z * gridDim.x * gridDim.y + blockIdx.y * gridDim.x + blockIdx.x;
 
   if (linearBlockIdx == bid) {
     unsigned int linearLocalThreadIdx = threadIdx.z * blockDim.x * blockDim.y +
                                         threadIdx.y * blockDim.x + threadIdx.x;
 
-    if ( linearLocalThreadIdx == tid) {
-      if ( sname && dname) {
-        printf("!C(%u,%u,%u:%u):%s[%lu]>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                       line, col,
-                                                       sname, soffset,
-                                                       dname, doffset,
-                                                       size);
-      }
-      else if ( sname) {
+    if (linearLocalThreadIdx == tid) {
+      if (sname && dname) {
+        printf("!C(%u,%u,%u:%u):%s[%lu]>%s[%lu]:%u\n", bid,
+               linearLocalThreadIdx, line, col, sname, soffset, dname, doffset,
+               size);
+      } else if (sname) {
         printf("!C(%u,%u,%u:%u):%s[%lu]>loc:%u\n", bid, linearLocalThreadIdx,
-                                                   line, col,
-                                                   sname, soffset,
-                                                   size);
-      }
-      else {
+               line, col, sname, soffset, size);
+      } else {
         printf("!C(%u,%u,%u:%u):loc>%s[%lu]:%u\n", bid, linearLocalThreadIdx,
-                                                   line, col,
-                                                   dname, doffset,
-                                                   size);
+               line, col, dname, doffset, size);
       }
     }
   }
